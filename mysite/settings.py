6fcd44a3f1952ef6d55e9b3ca0a79ea0d2d35ad0
Joseph Yeung <yeungjoseph10@gmail.com>
2017-12-15 01:33:41 -0800

    Fixed faulty import in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-#Import the secret key from another module
-from secret_settings import * 
 import os
+from .secret_settings import *
+
+
+#Import the secret key from another module
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = YOUR_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'josephyeung.pythonanywhere.com']
 
