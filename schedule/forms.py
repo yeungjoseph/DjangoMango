8ff5bffe3642e6b2d40cc6f9f69a957f70746c91
Joseph Yeung <yeungjoseph10@gmail.com>
2017-12-24 15:27:13 -0800

    Implemented a better datepicker and changed task name to task topic.

--- schedule/forms.py
+++ schedule/forms.py
@@ -5,10 +5,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
 	class Meta:
 		model = Task
-		fields = ('task_name', 'task_text', 'due_date')
+		fields = ('task_topic', 'task_text', 'due_date')
 		widgets = {
 			'task_text': forms.Textarea(attrs={'cols': 80, 'rows': 5}),
-			'due_date': forms.SelectDateWidget(
-				empty_label=('Year', 'Month', 'Day')
-			),
+			'due_date': forms.TextInput(attrs={'type': 'date'}),
 		}
--- schedule/models.py
+++ schedule/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-	task_name = models.CharField('Task', max_length=100)
+	task_topic = models.CharField('Topic', max_length=100)
 	task_text = models.TextField('Description', blank=True)
 	due_date = models.DateField('Finish by date', blank=True, null=True)
 	
